9e1117076eb526a1ae31264c25bb14a7a161eb03
magner35 <mr.jaedong@gmail.com>
2026-05-25 01:12:07 +0300

    feat: add assembly instructions and switch blink gpio to pe6

--- 3-blink/blink.s
+++ 3-blink/blink.s
@@ -8,17 +8,30 @@
 
 reset_exception_handler:
 
-// enable I/O port B clock
+//begin
+add r0, r1
+add r0, r0, r1
+
+and r0, r1
+ands r0, r0, r1
+ANDS r0, r0, r1
+
+adds r0, #1
+adds.n r0, #1
+adds.w r0, #1
+
+
+// enable I/O port E clock
 ldr r0, =0x40021000 + 0x18 // RCC_APB2ENR
 ldr r1, [r0]
-orr r1, 1 << 3 // IOPBEN
+orr r1, 1 << 6 // IOPEEN
 str r1, [r0]
 
-// configure PB2 as push-pull output with 2 MHz speed
-ldr r0, =0x40010C00 + 0x00 // GPIOB_CRL
+// configure PE6 as push-pull output with 2 MHz speed
+ldr r0, =0x40011800 + 0x00 // GPIOE_CRL
 ldr r1, [r0]
-bic r1, 0x0f << 8
-orr r1, 0b10 << 8 // CNF2:0
+bic r1, 0x0f << 24
+orr r1, 0b10 << 24 // CNF2:0
 str r1, [r0]
 
 blink_loop:
@@ -30,9 +43,9 @@ subs r0, 1
 bne delay_loop
 
 // toggle PB2
-ldr r0, =0x40010C00 + 0x0c // GPIOB_ODR
+ldr r0, =0x40011800 + 0x0c // GPIOE_ODR
 ldr r1, [r0]
-eor r1, 1 << 2 // ODR2
+eor r1, 1 << 6 // ODR6
 str r1, [r0]
 
 b blink_loop
