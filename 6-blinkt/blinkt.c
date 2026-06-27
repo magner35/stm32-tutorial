a19fa51ec3dc746944769ee3bea085e3ff0c7503
magner35 <mr.jaedong@gmail.com>
2026-06-27 23:59:41 +0300

    refactor: simplify register access with macros

--- 6-blinkt/blinkt.c
+++ 6-blinkt/blinkt.c
@@ -6,8 +6,34 @@ static void enable_port_clock(void);
 static void configure_pin(void);
 static void configure_sys_tick_timer(void);
 
+#define RCC_BASE_ADDRESS 0x40021000
+#define RCC_APB2ENR (*(volatile uint32_t *)(RCC_BASE_ADDRESS + 0x18))
+
+#define RCC_APB2ENR_IOPBEN 3
+
+#define GPIOB_BASE_ADDRESS 0x40010C00
+#define GPIOB_CRL (*(volatile uint32_t *)(GPIOB_BASE_ADDRESS + 0x00))
+#define GPIOB_ODR (*(volatile uint32_t *)(GPIOB_BASE_ADDRESS + 0x0C))
+
+#define GPIOX_ODR_ODR2 2
+
+#define GPIOX_CRL_MODE2_0 8
+#define GPIOX_CRL_MODE2_1 9
+#define GPIOX_CRL_CNF2_0 10
+#define GPIOX_CRL_CNF2_1 11
+
+#define STK_BASE_ADDRESS 0xE000E010
+#define STK_CTRL (*(volatile uint32_t *)(STK_BASE_ADDRESS + 0x00))
+#define STK_LOAD (*(volatile uint32_t *)(STK_BASE_ADDRESS + 0x04))
+
+#define STK_CTRL_ENABLE 0
+#define STK_CTRL_TICKINT 1
+#define STK_CTRL_CLKSOURCE 2
+#define STK_CTRL_COUNTFLAG 16
+
 void start(void)
 {
+
     enable_port_clock();
     configure_pin();
     configure_sys_tick_timer();
@@ -20,55 +46,21 @@ void start(void)
 static void enable_port_clock(void)
 {
     // enable I/O port B clock
-    uint32_t rcc_base_address = 0x40021000;
-    uint32_t rcc_apb2enr_address = rcc_base_address + 0x18;
-    uint32_t rcc_apb2enr_iopben = 1 << 3;
-    volatile uint32_t *rcc_apb2enr_pointer = (uint32_t *)rcc_apb2enr_address;
-    uint32_t rcc_apb2enr_value = *rcc_apb2enr_pointer;
-    rcc_apb2enr_value |= rcc_apb2enr_iopben;
-    *rcc_apb2enr_pointer = rcc_apb2enr_value;
+    RCC_APB2ENR = (1 << RCC_APB2ENR_IOPBEN);
 }
 
 static void configure_pin(void)
 {
     // configure PB2 as push-pull output with 2 MHz speed
-    uint32_t gpiob_base_address = 0x40010C00;
-    uint32_t gpiob_crl_address = gpiob_base_address + 0x00;
-    uint32_t gpiox_crl_mode2_0 = 1 << 8;
-    uint32_t gpiox_crl_mode2_1 = 1 << 9;
-    uint32_t gpiox_crl_cnf2_0 = 1 << 10;
-    uint32_t gpiox_crl_cnf2_1 = 1 << 11;
-
-    volatile uint32_t *gpiob_crl_pointer = (uint32_t *)gpiob_crl_address;
-    uint32_t gpiob_crl_value = *gpiob_crl_pointer;
-    gpiob_crl_value &= ~gpiox_crl_mode2_0;
-    gpiob_crl_value |= gpiox_crl_mode2_1;
-    gpiob_crl_value &= ~gpiox_crl_cnf2_0;
-    gpiob_crl_value &= ~gpiox_crl_cnf2_1;
-    *gpiob_crl_pointer = gpiob_crl_value;
+    GPIOB_CRL = (1 << GPIOX_CRL_MODE2_1);
 }
 
 static void configure_sys_tick_timer(void)
 {
-    uint32_t stk_base_address = 0xE000E010;
-    uint32_t stk_ctrl_address = stk_base_address + 0x00;
-    uint32_t stk_ctrl_enable = 1 << 0;
-    uint32_t stk_ctrl_tickint = 1 << 1;
-
-    uint32_t stk_load_address = stk_base_address + 0x04;
-
     // enable systick exception, enable systick counter
-    volatile uint32_t *stk_ctrl_pointer = (uint32_t *)stk_ctrl_address;
-    uint32_t stk_ctrl_value = *stk_ctrl_pointer;
-    stk_ctrl_value |= stk_ctrl_enable;
-    stk_ctrl_value |= stk_ctrl_tickint;
-    *stk_ctrl_pointer = stk_ctrl_value;
-
+    STK_CTRL = (1 << STK_CTRL_ENABLE) | (1 << STK_CTRL_TICKINT) | (1 << STK_CTRL_CLKSOURCE);
     // set reload value to 500 ms
-    volatile uint32_t *stk_load_pointer = (uint32_t *)stk_load_address;
-    uint32_t stk_load_value;
-    stk_load_value = 8000000 / 8 / 2;
-    *stk_load_pointer = stk_load_value;
+    STK_LOAD = 8000000 / 2;
 }
 
 static void toggle_pin(void);
@@ -81,12 +73,5 @@ void sys_tick_exception_handler(void)
 static void toggle_pin(void)
 {
     // toggle PB2
-    uint32_t gpiob_base_address = 0x40010C00;
-    uint32_t gpiob_odr_address = gpiob_base_address + 0x0c;
-    uint32_t gpiox_odr_odr2 = 1 << 2;
-
-    volatile uint32_t *gpioc_odr_pointer = (uint32_t *)gpiob_odr_address;
-    uint32_t gpioc_odr_value = *gpioc_odr_pointer;
-    gpioc_odr_value ^= gpiox_odr_odr2;
-    *gpioc_odr_pointer = gpioc_odr_value;
+    GPIOB_ODR ^= (1 << GPIOX_ODR_ODR2);
 }
